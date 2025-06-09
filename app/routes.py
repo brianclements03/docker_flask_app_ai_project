679693043867b109a0938be7c92a7798a14d65a1
Brian Clements <brian.l.clements03@gmail.com>
2025-06-08 21:02:12 -0500

    added functionality to parse the uploaded file and append the rows to the table

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,16 @@
 from flask import Blueprint, current_app, render_template
 from app.models import db, MyTable #adjust model name as needed
 import os
-from flask import request, redirect, url_for, flash
+from flask import request, redirect, url_for, flash, current_app
+import pandas as pd
+from app.models import db, MyTable
+import openpyxl
 
+ALLOWED_EXTENSIONS = {'xlsx'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
 main = Blueprint('main', __name__)
 
@@ -32,20 +40,44 @@ def upload_file():
         return redirect(url_for('main.home'))
 
     file = request.files['file']
+
     if file.filename == '':
         # return "No selected file", 400 #ditto above
         flash("No selected file")
         return redirect(url_for('main.home'))
-
+    
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
     filepath = os.path.join(UPLOAD_FOLDER, file.filename)
     file.save(filepath)
+    
+    # if file and allowed_file(file.filename):
+    #     filename = secure_filename(file.filename)
+    #     filepath = os.path.join(UPLOAD_FOLDER, filename)
+    #     file.save(filepath)
 
     # return f"File '{file.filename}' uploaded successfully!"
-    flash(f"File '{file.filename}' uploaded successfully!")
+    # flash(f"File '{file.filename}' uploaded successfully!")
+    # return redirect(url_for('main.home'))
+
+    #parse the newly uploaded data file
+    try:
+        df = pd.read_excel(filepath, engine="openpyxl")
+
+        for _, row in df.iterrows():
+            user = MyTable(name=row['name'], email=row['email'])
+            db.session.add(user)
+
+        db.session.commit()
+        flash(f"File '{file.filename}' uploaded and {len(df)} records inserted!", "success")
+    except Exception as e:
+        flash(f"Failed to process file: {e}")
+        # return redirect(url_for('main.home'))
+    
     return redirect(url_for('main.home'))
 
-
+    # else:
+    #     flash('Invalid file type. Only .xlsx files are allowed')
+    #     return redirect(url_for('main.home'))
 
 
 
