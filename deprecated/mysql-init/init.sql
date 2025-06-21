6619a4106d5b0c42dd48259da82791838db84186
Brian Clements <brian.l.clements03@gmail.com>
2025-06-21 12:31:04 -0500

    deleted unecessary files

--- deprecated/mysql-init/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS my_table (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE
-);
-
-INSERT INTO my_table (name, email) VALUES
-('Alice Example', 'alice@example.com'),
-('Bob Smith', 'bob@example.com');
--- deprecated/run.py
+++
@@ -1,8 +0,0 @@
-# the 'entry point' for the app
-
-from app import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(app.config['PORT']), debug=True)
